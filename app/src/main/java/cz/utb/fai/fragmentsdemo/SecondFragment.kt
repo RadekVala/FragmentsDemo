e49af3029fefb02e6804d3b788f613f22d0cf7c8
Radek Vala <radek.www@gmail.com>
2023-10-19 10:49:17 +0200

    Initial commit

---
+++ app/src/main/java/cz/utb/fai/fragmentsdemo/SecondFragment.kt
@@ -0,0 +1,52 @@
+package cz.utb.fai.fragmentsdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import cz.utb.fai.fragmentsdemo.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    val args: SecondFragmentArgs  by navArgs()
+
+
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textviewSecond.text = args.counter.toString()
+
+        binding.buttonSecond.setOnClickListener {
+            //findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            findNavController().navigate(R.id.action_SecondFragment_to_placeholder)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
